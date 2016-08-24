b3fde44acbcd53aa3494ffc045fee3dfac52538f
danthedapperman <danthedapperman@users.noreply.github.com>
2016-08-24 15:56:13 -0700

    changes for backporting from py3.6 to py2.7
    These changes are to get the code to run in Python2.7 which is the version supported by AWS Lambda. This code was originally developed in Python3.6

--- Scratch code.py
+++ Scratch code.py
@@ -2,10 +2,9 @@
 #import os
 import esdclasses
 import csv
+import UserDict
 
-"""
-# Loop through the data read from csv and dump the first bit of it to screen
-with open('EOSL.csv', mode='r', newline=None) as csvfile:
+with open('EOSL.csv', mode='rU') as csvfile:  #mode U is deprecated in Python 3.6
 	reader = csv.DictReader(csvfile, dialect='excel')
 	i=0
 	#begin supportdates
@@ -31,24 +30,22 @@ with open('EOSL.csv', mode='r', newline=None) as csvfile:
 	print(']')
 	#end supportdates
 	csvfile.close()
-"""	
-
-"""
-Read data from CSV file into dictionary objects
-
-Initilize utility data structures from the dictionary objects
-
-Loop through the data read from csv and write it as JSON to file & screen	
-"""
-with open ('EOSL.csv', mode='r', newline=None) as csvfile:
-	field_names = esdclasses.DataPoint.getfieldnames()
-	reader = csv.DictReader(csvfile, dialect='excel', fieldnames=field_names)
-	writer = csv.DictWriter(csvfile, fieldnames=field_names)
+	
+	
+with open ('EOSL.csv', mode='rU') as csvfile:  #mode U is deprecated in Python 3.6
+        dictobj = UserDict.IterableUserDict( {'Product' : ' ', 'Model' : ' ',
+                        'GA Date' : ' ', 'EOL Date' : ' ', 'EOPS Date' : ' ',
+                        'EOSL Date' : ' ',
+                        'GA Updated?' : ' ', 'EOL Updated?' : ' ',
+                        'EOPS Updated?' : ' ', 'EOSL Updated?' : ' '} )
+        dpo = esdclasses.DataPoint(dictobj)
+	field_names = dpo.getfieldnames
+	reader = csv.DictReader(csvfile, dialect='excel')
 	
 	alldata = esdclasses.EMCSupportDates()
 	alldata.setlastupdatetonow()
 	
-	with open ('emcsupportdata.json', mode='w') as jsonfile:
+	with open ('emcsupportdata.json', mode='w') as jsonfile: #mode U is deprecated in Python 3.6
 		i=0
 		
 		#begin supportdates
@@ -57,20 +54,12 @@ with open ('EOSL.csv', mode='r', newline=None) as csvfile:
 			
 			# skip the first one because it will be the header row
 			if i > 1:
-				#begin datapoint
 				thisone = esdclasses.DataPoint(row)
 				alldata.push(thisone)
-				"""
-				#experimental JSON library code -- never worked
-				#json.dump('"datapoint": ', jsonfile, indent="\t\t")
-				#json.dump(row, jsonfile, indent="\t\t\t")
-				#json.dump('"product":' row['Product'], jsonfile, indent="\t\t\t")		
-				#json.dump('\n', jsonfile)
-				"""
-				#end datapoint
+		
+
 		#end supportdates
 
-		#io.TextIOWrapper(jsonfile).write(alldata.toJSON(0))
 		jsonfile.write(alldata.toJSON(0))
 		print(alldata.toJSON(0))
 		
