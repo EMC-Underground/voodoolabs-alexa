8845b108e923be6b6b0c5fb6624b058881c3d7ea
AdamPippert <pippea@USSAPIPPEAL1C.corp.emc.com>
2016-10-05 15:53:31 -0700

    syntax error

--- esdclasses.py
+++ esdclasses.py
@@ -273,7 +273,7 @@ class DataPoint:
 		simplestring = ""
 		simplestring = simplestring + self.getproduct() + ' ' +  self.getmodel() + ' has the following support dates: '
 		simplestring = simplestring + 'GA Date: ' + self.fill_string_if_empty(self.getgadate()) + ', '
-		simplestring = simplestring + ('EOL Date: ' + self.fill_string_if_empty(self.geteoldate()) + ', '
+		simplestring = simplestring + 'EOL Date: ' + self.fill_string_if_empty(self.geteoldate()) + ', '
 		simplestring = simplestring + 'EOPS Date: ' + self.fill_string_if_empty(self.geteopsdate()) + ', '
 		simplestring = simplestring + 'EOSL Date: ' + self.fill_string_if_empty(self.geteosldate())
 		return simplestring
